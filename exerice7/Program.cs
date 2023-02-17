672c5f3274973eb556ec67a0b5fbeca601a5a32b
denis levanchuk <levanchuk.d@mail.ru>
2023-02-17 18:12:34 +0600

    exerice7

---
+++ exerice7/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите число N: ");
+        int n = int.Parse(Console.ReadLine());
+
+        Console.WriteLine($"Четные числа от 1 до {n}:");
+        for (int i = 2; i <= n; i += 2)
+        {
+            Console.Write($"{i} ");
+        }
